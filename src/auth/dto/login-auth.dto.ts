0ed2cd4206a7beec744df0d1a0c699bc16887b17
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2026-04-16 22:12:13 -0300

    feat: added Apiproperty into login-auth.dto

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginAuthDto {
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'Email of the user',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'strongpassword123',
+    description: 'Password of the user',
+  })
   @IsString()
   @MinLength(8)
   @Transform(({ value }) => value.trim())
   password: string;
 }
+
+
