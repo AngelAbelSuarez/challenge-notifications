d52467a8b2589868caed32c0b26fe016a3ec0c30
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2026-04-18 01:02:45 -0300

    feat: emial provider

---
+++ src/notifications/providers/email.provider.ts
@@ -0,0 +1,52 @@
+import { Injectable } from "@nestjs/common";
+import { NotificationProvider, SendResult } from "../interfaces/notification-provider.interface";
+import { ChannelType } from "../enums/channel-type.enum";
+import { isEmail } from "class-validator";
+
+
+@Injectable()
+export class EmailProvider implements NotificationProvider {
+
+    public readonly channel: ChannelType.EMAIL;
+
+    validateRecipient(recipient: string): boolean {
+        return isEmail(recipient);
+    }
+
+    formatContent(content: string): string {
+        return `
+            <html>
+                <body>
+                    <h2>Notificación</h2>
+                    <p>${content}</p>
+                </body>
+            </html>
+        `;
+    }
+
+    async send(recipient: string, content: string): Promise<SendResult> {
+        if (!this.validateRecipient(recipient)) {
+            return {
+                success: false,
+                message: 'Invalid email'
+            }
+        }
+
+        const formattedContent = this.formatContent(content);
+
+        console.log(`[EMAIL] Sending to: ${recipient}`);
+        console.log(`[EMAIL] Template generated`);
+        console.log(`[EMAIL] Sending...`);
+
+        return {
+            success: true,
+            message: 'Email sent successfully',
+            metadata: {
+                recipient,
+                template: formattedContent,
+                sentAt: new Date()
+            }             
+        }
+    }
+
+}
