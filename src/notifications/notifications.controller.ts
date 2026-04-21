2aa3560a2064218abcb291676133ebf7dd0cc158
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2026-04-20 21:45:07 -0300

    feat: controller create notification

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -6,22 +6,28 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
 import { SendResult } from './interfaces/notification-provider.interface';
+import { AuthGuard } from '@/auth/guard/auth.guard';
+import { ActiveUser } from '@/common/decorators/active-user.decorator';
+import type { ActiveUserInterface } from '@/common/interfaces/active-user.interface'; 
 
 @Controller('notifications')
+@UseGuards(AuthGuard)
 export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) {}
+  constructor(private readonly notificationsService: NotificationsService) { }
 
   @Post()
   create(
     @Body() createNotificationDto: CreateNotificationDto,
-    @Param('userId') userId: string,
+    @ActiveUser() activeUser: ActiveUserInterface,
   ): Promise<SendResult> {
-    return this.notificationsService.create(createNotificationDto, userId);
+    console.log('activeUser.id',activeUser.id);
+    return this.notificationsService.create(createNotificationDto, activeUser.id);
   }
 
   @Get()
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,15 +3,16 @@ import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notifications } from './entities/notification.entity';
-// import { Users } from '@/users/entities/user.entity';
+import { Users } from '@/users/entities/user.entity';
 import { NotificationsRepository } from './notifications.repository';
 import { NotificationProviderFactory } from './providers/provider.factory';
 import { EmailProvider } from './providers/email.provider';
 import { SmsProvider } from './providers/sms.provider';
 import { PushProvider } from './providers/push.provider';
+import { AuthModule } from '@/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notifications])],
+  imports: [TypeOrmModule.forFeature([Notifications, Users]), AuthModule],
   controllers: [NotificationsController],
   providers: [
     NotificationsRepository,
